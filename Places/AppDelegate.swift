e7f8084479b372bfcf23d960731701d851965775
buddybuild <help@buddybuild.com>
2017-03-03 18:46:42 -0800

    Adding buddybuild SDK
    - Add BuddyBuildSDK.framework
    - Initialize in App Delegate

--- Places/AppDelegate.swift
+++ Places/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    BuddyBuildSDK.setup()
+    
     return true
   }
   
